e2c0eef8d187e06dc0a4d40bd022dd2313d9549c
Orisano <weekendsummer.endless@gmail.com>
2016-04-08 14:30:26 +0900

    add subscribe

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const sub = io.of("/subscribe");
+
+const Status = Object.freeze({
+    CLOSE: 0,
+    OPEN: 1,
+    RUNNING: 2
+});
+
+let status = Status.CLOSE;
+const logs = [""];
 
 server.listen(8080);
 
@@ -10,23 +20,30 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-const Status = Object.freeze({
-    CLOSE: 0,
-    OPEN: 1,
-    RUNNING: 2
+app.get("/latest", (req, res) => {
+    res.send(logs[logs.length - 1]);
 });
-let status = Status.CLOSE;
 
 io.on("connection", (socket) => {
     status = Status.OPEN;
     console.log("[INFO] connection established.");
     socket.on("result", (data) => {
-       console.log(data);
         status = Status.OPEN;
+        console.log(data);
+        logs.push(data);
+        sub.emit("data", data);
+
     });
     socket.on("disconnect", () => {
-        status = status.CLOSE;
+        status = Status.CLOSE;
         console.log("[INFO] disconnected.");
+    })
+});
+
+sub.on("connection", (socket) => {
+    console.log("[INFO] subscriber registered.");
+    socket.on("disconnect", () => {
+        console.log("[INFO] subscriber disconnected.");
     })
 });
 
